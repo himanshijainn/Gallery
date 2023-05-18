92cd9cc1969d23062970c9dae2342e71fcabc621
himanshijainn <himanshijain053@gmail.com>
2023-05-18 18:44:13 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var angle = 0;
-function galleryspin(sign) { 
-spinner = document.querySelector("#spinner");
-if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
-spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
-}
+// function galleryspin(sign) { 
+// spinner = document.querySelector("#spinner");
+// if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+// spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+// }
+
+
+function galleryspin() { 
+    spinner = document.querySelector("#spinner");
+    angle = angle - 45
+    spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+    }
+
+    function setIntervalWithDelay(callback, delay) {
+        function interval() {
+          callback();
+          setTimeout(interval, delay);
+        }
+        
+        setTimeout(interval, delay);
+      }
+      
+      setIntervalWithDelay(galleryspin, 1000);    
+    
